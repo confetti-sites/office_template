4150d1840922a4c32c00557e232e0bbc544bdfcb
reindert-vetter <reindertvetter@gmail.com>
2023-05-07 00:38:37 +0200

    Pricing text

--- view/pricing.blade.php
+++ view/pricing.blade.php
@@ -7,18 +7,197 @@ class="relative z-20 overflow-hidden bg-white pt-20 pb-12 lg:pt-[120px] lg:pb-[9
   <div class="-mx-4 flex flex-wrap">
     <div class="w-full px-4">
       <div class="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-        <span class="text-primary mb-2 block text-lg font-semibold">
+        <h2 class="text-primary mb-2 block text-lg font-semibold">
           Pricing Table
-        </span>
-        <h2
+        </h2>
+        <div
           class="text-dark mb-4 text-3xl font-bold sm:text-4xl md:text-[40px]"
         >
           Only pay for hosting
-        </h2>
+        </div>
         <p class="text-body-color text-base">
-          There are many variations of passages of Lorem Ipsum available but
-          the majority have suffered alteration in some form.
+          Confetti offers various solutions to help you build websites faster. Trying it out is free. When you are ready to publish your website, you only pay for hosting.
         </p>
+      </div>
+    </div>
+  </div>
+  <div class="text-center py-4 lg:px-4">
+    <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+      <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Beta</span>
+      <span class="font-semibold mr-2 text-left flex-auto">Since Confetti is in beta, no costs will be charged.</span>
+      <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+    </div>
+  </div>
+  <div class="-mx-4 flex flex-wrap justify-center">
+    <div class="w-full px-4">
+      <div
+              class="border-primary shadow-pricing relative z-10 mb-10 overflow-hidden rounded-xl border border-opacity-20 bg-white py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12"
+      >
+        <h2 class="text-primary mb-4 block text-lg font-semibold">
+          Learning, testing, and developing
+        </h2>
+        <span class="text-dark mb-5 text-[42px] font-bold">
+          Free
+        </span>
+        <p
+                class="text-body-color mb-8 text-base"
+        >
+          Confetti-CMS is free to use for tinkering your next idea.
+        </p>
+        <a
+                href="javascript:void(0)"
+                class="bg-primary border-primary block w-full rounded-md border p-4 text-center text-base font-semibold text-white transition hover:bg-opacity-90"
+        >
+          Get started
+        </a>
+        <div>
+          <span class="absolute right-0 top-7 z-[-1]">
+            <svg
+                    width="77"
+                    height="172"
+                    viewBox="0 0 77 172"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="86" cy="86" r="86" fill="url(#paint0_linear)" />
+              <defs>
+                <linearGradient
+                        id="paint0_linear"
+                        x1="86"
+                        y1="0"
+                        x2="86"
+                        y2="172"
+                        gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#3056D3" stop-opacity="0.09" />
+                  <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          <span class="absolute right-4 top-4 z-[-1]">
+            <svg
+                    width="41"
+                    height="89"
+                    viewBox="0 0 41 89"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                      cx="38.9138"
+                      cy="87.4849"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 87.4849)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="74.9871"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 74.9871)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="62.4892"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 62.4892)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="38.3457"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 38.3457)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="13.634"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 13.634)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="50.2754"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 50.2754)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="26.1319"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 26.1319)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="38.9138"
+                      cy="1.42021"
+                      r="1.42021"
+                      transform="rotate(180 38.9138 1.42021)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="87.4849"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 87.4849)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="74.9871"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 74.9871)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="62.4892"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 62.4892)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="38.3457"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 38.3457)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="13.634"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 13.634)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="50.2754"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 50.2754)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="26.1319"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 26.1319)"
+                      fill="#3056D3"
+              />
+              <circle
+                      cx="26.4157"
+                      cy="1.4202"
+                      r="1.42021"
+                      transform="rotate(180 26.4157 1.4202)"
+                      fill="#3056D3"
+              />
+            </svg>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -27,41 +206,30 @@ class="relative z-20 overflow-hidden bg-white pt-20 pb-12 lg:pt-[120px] lg:pb-[9
       <div
         class="border-primary shadow-pricing relative z-10 mb-10 overflow-hidden rounded-xl border border-opacity-20 bg-white py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12"
       >
-        <span class="text-primary mb-4 block text-lg font-semibold">
+        <h2 class="text-primary mb-4 block text-lg font-semibold">
           Personal & small business
-        </span>
-        <h2 class="text-dark mb-5 text-[42px] font-bold">
-          €14
-          <span class="text-body-color text-base font-medium"> / month </span>
         </h2>
+        <div class="text-dark mb-5 text-[42px] font-bold">
+          €10
+          <span class="text-body-color text-base font-medium"> / month for your first server</span>
+        </div>
         <p
           class="text-body-color mb-8 border-b border-[#F2F2F2] pb-8 text-base"
         >
-          Perfect for a website for a local club, small business or a busy blog."
+          A website for a local club, small business or a busy blog.
         </p>
         <div class="mb-7">
-          <p class="text-body-color mb-1 text-base leading-loose">1 User</p>
-          <p class="text-body-color mb-1 text-base leading-loose">
-            All UI components
-          </p>
-          <p class="text-body-color mb-1 text-base leading-loose">
-            Lifetime access
-          </p>
-          <p class="text-body-color mb-1 text-base leading-loose">
-            Free updates
-          </p>
-          <p class="text-body-color mb-1 text-base leading-loose">
-            Use on 1 (one) project
-          </p>
-          <p class="text-body-color mb-1 text-base leading-loose">
-            SSL-certificate, test environments, caching, backups. All managed by us.
-          </p>
+          <p class="text-body-color mb-1 text-base leading-loose">Backups</p>
+          <p class="text-body-color mb-1 text-base leading-loose">SSL-certificate</p>
+          <p class="text-body-color mb-1 text-base leading-loose">Multiple domains</p>
+          <p class="text-body-color mb-1 text-base leading-loose">Test environments</p>
+          <p class="text-body-color mb-1 text-base leading-loose">Secured with Auth0</p>
         </div>
         <a
           href="javascript:void(0)"
           class="text-primary hover:bg-primary hover:border-primary block w-full rounded-md border border-[#D4DEFF] bg-transparent p-4 text-center text-base font-semibold transition hover:text-white"
         >
-          Choose Personal
+          Publish your website
         </a>
         <div>
           <span class="absolute right-0 top-7 z-[-1]">
@@ -264,62 +432,6 @@ class="relative z-20 overflow-hidden bg-white pt-20 pb-12 lg:pt-[120px] lg:pb-[9
                 transform="rotate(180 13.9177 1.42019)"
                 fill="#3056D3"
               />
-              <circle
-                cx="1.41963"
-                cy="87.4849"
-                r="1.42021"
-                transform="rotate(180 1.41963 87.4849)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="74.9871"
-                r="1.42021"
-                transform="rotate(180 1.41963 74.9871)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="62.4892"
-                r="1.42021"
-                transform="rotate(180 1.41963 62.4892)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="38.3457"
-                r="1.42021"
-                transform="rotate(180 1.41963 38.3457)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="13.634"
-                r="1.42021"
-                transform="rotate(180 1.41963 13.634)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="50.2754"
-                r="1.42021"
-                transform="rotate(180 1.41963 50.2754)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="26.1319"
-                r="1.42021"
-                transform="rotate(180 1.41963 26.1319)"
-                fill="#3056D3"
-              />
-              <circle
-                cx="1.41963"
-                cy="1.4202"
-                r="1.42021"
-                transform="rotate(180 1.41963 1.4202)"
-                fill="#3056D3"
-              />
             </svg>
           </span>
         </div>
@@ -329,33 +441,32 @@ class="relative z-20 overflow-hidden bg-white pt-20 pb-12 lg:pt-[120px] lg:pb-[9
       <div
         class="border-primary shadow-pricing relative z-10 mb-10 overflow-hidden rounded-xl border border-opacity-20 bg-white py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12"
       >
-        <span class="text-primary mb-4 block text-lg font-semibold">
+        <h2 class="text-primary mb-4 block text-lg font-semibold">
           Business
-        </span>
-        <h2 class="text-dark mb-5 text-[42px] font-bold">
-          + €24
-          <span class="text-body-color text-base font-medium"> / month </span>
         </h2>
+        <div class="text-dark mb-5 text-[42px] font-bold">
+          + €24
+          <span class="text-body-color text-base font-medium"> / month for each extra server</span>
+        </div>
         <p class="text-body-color mb-8 border-b border-[#F2F2F2] pb-8 text-base">
-            Do you have a high traffic website? Let us handle the stress for you.
+            Do you have a high traffic website? Let us handle the stress for you. So you can scale your business.
         </p>
         <div class="mb-7">
-          <p class="text-body-color mb-1 text-base leading-loose">5 Users</p>
           <p class="text-body-color mb-1 text-base leading-loose">
-            All UI components
+            Auto scale
           </p>
           <p class="text-body-color mb-1 text-base leading-loose">
-            Free updates
+            Multi region
           </p>
           <p class="text-body-color mb-1 text-base leading-loose">
-            Always
+            Priority support
           </p>
         </div>
         <a
-          href="javascript:void(0)"
-          class="bg-primary border-primary block w-full rounded-md border p-4 text-center text-base font-semibold text-white transition hover:bg-opacity-90"
+                href="javascript:void(0)"
+                class="text-primary hover:bg-primary hover:border-primary block w-full rounded-md border border-[#D4DEFF] bg-transparent p-4 text-center text-base font-semibold transition hover:text-white"
         >
-          Choose Business
+          Enable auto-scaling
         </a>
         <div>
           <span class="absolute right-0 top-7 z-[-1]">
