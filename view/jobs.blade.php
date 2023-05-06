2f49cc70c439261062c13873eb1c1b4758eb2038
reindert-vetter <reindertvetter@gmail.com>
2023-05-06 13:31:34 +0200

    Fix multiple lists

--- view/jobs.blade.php
+++ view/jobs.blade.php
@@ -1,6 +1,13 @@
-@php($jobs = section('jobs'))
+@php($jobs = section('job_board'))
 <ul>
-    @foreach($jobs->list('job') as $job)
+    @foreach($jobs->list('job1') as $job)
         <li>{{ $job->text('name')->default('default name') }}</li>
     @endforeach
 </ul>
+
+<ul>
+    @foreach($jobs->list('job2') as $job)
+        <li>{{ $job->text('name')->default('default name') }}</li>
+    @endforeach
+</ul>
+
