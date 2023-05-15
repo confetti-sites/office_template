d6c0f66c79e6bbb83b021c67223064df598bd884
reindert-vetter <reindertvetter@gmail.com>
2023-05-15 08:56:27 +0200

    get value from select

--- view/demo.blade.php
+++ view/demo.blade.php
@@ -3,13 +3,13 @@
 <div class="dark:bg-gray-900">
     @foreach($demo->list('home_blocks')->columns(['title'])->min(1)->max(6) as $block)
         <div class="container py-4 md:flex">
-            @php($position = $block->select('position')->options(['left', 'right'])->default('left'))
-            @if($position->get() == 'left')
+            @php($position = $block->select('example_position')->options(['left', 'right'])->default('right'))
+            @if($position == 'left')
                 <div class="md:w-1/2 mt-8 md:mt-0 md:mr-12 opacity-0 py-2" x-intersect="$el.classList.add('slide-in-left')">
                     <img src="https://cdn.tuk.dev/previews/desktop-2x/hero_7.jpg" alt="">
                 </div>
             @endif
-            <div class="md:w-1/2 opacity-0 py-2" x-intersect="$el.classList.add('slide-in-left')">
+            <div class="md:w-1/2 opacity-0 py-2" x-intersect="$el.classList.add('slide-in-{{ $position }}')">
                 <h2 class="text-2xl dark:text-white text-gray-900">{{ $block->text('title')->min(1)->max(100) }}</h2>
                 <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400 font-body">
                     {{ $block->text('description')->min(1)->max(600) }}
