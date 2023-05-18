eb0e907c0b38ef44da1941ea094eef33b9beb148
reindert-vetter <reindertvetter@gmail.com>
2023-05-18 22:59:13 +0200

    Generate id part by ulid

--- structure/list_component.blade.php
+++ structure/list_component.blade.php
@@ -5,8 +5,8 @@
         name: 'list',
         decorations: [
             Decoration::MIN->comment('Minimum number of items'),
-            Decoration::MAX->comment('Minimum number of items'),
-            Decoration::COLUMNS->comment('This becomes the the headers of the table in de admin'),
+            Decoration::MAX->comment('Maximum number of items'),
+            Decoration::COLUMNS->comment('This becomes the headers of the table in de admin'),
         ],
         phpClass: file_get_contents(repositoryPath() . '/structure/list_component.class.php'),
 
--- view/faq.blade.php
+++ view/faq.blade.php
@@ -1,10 +1,10 @@
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-white dark:bg-gray-900 dark:text-white">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Frequently asked questions</h2>
+        <h2 class="mb-8 text-4xl tracking-tight font-extrabold">Frequently asked questions</h2>
         <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
             <div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         Do you offer a plan for students?
                     </h3>
@@ -13,7 +13,7 @@
                     </p>
                 </div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         Can I make my own plugins/modules?
                     </h3>
@@ -24,14 +24,14 @@
             </div>
             <div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         How much CPU do I get for one server?
                     </h3>
                     <p class="text-gray-500 dark:text-gray-400">Not all CPUs are the same. That's why it can be misleading to express CPU and memory in numbers. But we ensure you that our servers are optimized to provide the best performance for your website. With one server, you can easily run a website with low to moderate traffic. And if you need to handle more traffic, our caching solutions can help you scale up without any hassle. Regardless, implementing servers will always be more cost-effective than integrating with AWS.</p>
                 </div>
                 <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <h3 class="flex items-center mb-4 text-lg font-medium">
                         <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                         How long will Confetti remain completely free?
                     </h3>
