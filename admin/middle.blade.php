0a917dcb4b7d0c64c002b16ce2b59413011266d5
reindert-vetter <reindertvetter@gmail.com>
2023-05-21 22:25:41 +0200

    Fix go back

--- admin/middle.blade.php
+++ admin/middle.blade.php
@@ -13,7 +13,7 @@
     $components = $componentStore->whereParentKey($componentKey);
 @endphp
 <div class="container px-6 mx-auto grid">
-    @if($parentContentId !== '/section')
+    @if($parentContentId && $parentContentId !== '/section')
         <a
                 class="flex items-center justify-between w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                 href="/admin{{ $parentContentId }}"
