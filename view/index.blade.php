1a81a782f789839c7264c11f9b5aeb480b1e368d
Reindert <16726304+reindert-vetter@users.noreply.github.com>
2023-08-12 14:29:41 +0200

    Update index.blade.php

--- view/index.blade.php
+++ view/index.blade.php
@@ -2,7 +2,7 @@
 <head>
     <title>Confetti CMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/generated/view/tailwind.output.css"/>
+    <link rel="stylesheet" href="/shared_resources/tailwind/tailwind.output.css"/>
     <link rel="stylesheet" href="/view/assets/css/fonts.css"/>
     <script defer>
         @stack('scripts_*')
