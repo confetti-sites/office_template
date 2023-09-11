21dbfc3870cfb20aa41c90fadb4a26c70d48257b
reindert-vetter <reindertvetter@gmail.com>
2023-09-11 17:25:45 +0200

    Revert "Revert "One file for component""
    This reverts commit 4c30887d600407967255c0d0655ef7397be71268.

--- admin/structure/image_component.blade.php
+++ admin/structure/image_component.blade.php
@@ -1,14 +1,64 @@
-@php
-    use Confetti\Helpers\ComponentGenerator;
-    use Confetti\Helpers\Decoration;
-    echo(new ComponentGenerator(
-        name: 'image',
-        decorations: [
-            Decoration::LABEL->comment('Label is used as a field title in the admin panel'),
-            Decoration::HEIGHT->comment('Height of the image'),
-            Decoration::WIDTH->comment('Width of the image'),
-        ],
-        phpClass: file_get_contents(repositoryPath() . '/admin/structure/image_component.class.php'),
-    ))->toJson();
-@endphp
+@pushonce('image_component_config')
+    @php
+        echo(new Confetti\Helpers\ComponentGenerator(
+            // Example: $var->image('image')
+            name: 'image',
+            // Example: ->label('Image')->width(300)->height(200)
+            decorations: [
+                Confetti\Helpers\Decoration::LABEL->comment('Label is used as a field title in the admin panel'),
+                Confetti\Helpers\Decoration::HEIGHT->comment('Height of the image'),
+                Confetti\Helpers\Decoration::WIDTH->comment('Width of the image'),
+            ],
+        ))->toJson();
+    @endphp
+@endpushonce
 
+@pushonce('image_component_getter')
+    @php
+        ob_start();
+            new class extends Confetti\Helpers\ComponentStandard {
+                public function get(): string
+                {
+                    // Get saved value
+                    $content = $this->contentStore->find($this->id);
+                    if ($content !== null) {
+                        return $content->value;
+                    }
+
+                    // Get default value
+                    $component = $this->componentStore->findOrNull($this->key);
+                    $width = $component?->getDecoration('width')['value'] ?? 300;
+                    $height = $component?->getDecoration('height')['value'] ?? 200;
+
+                    return "https://picsum.photos/$width/$height?random=" . rand(0, 10000);
+                }
+            };
+        echo ob_get_clean();
+    @endphp
+@endpushonce
+
+@pushonce('image_component_field')
+    @php
+        /**
+         * @var \Confetti\Helpers\ComponentStore $componentStore
+         * @var \Confetti\Helpers\ComponentEntity $component
+         * @var \Confetti\Helpers\ContentStore $contentStore
+         * @var string $contentId
+         */
+    @endphp
+    <label class="block mt-4 text-sm">
+    <span class="">
+        {{ $component->getDecoration('label')['value'] }}
+    </span>
+        <input
+                type="file"
+                x-bind="field"
+                name="{{ $contentId }}"
+                value="{{ $contentStore->find($component->key) }}"
+        >
+    </label>
+@endpushonce
+
+@pushonce('image_component_preview')
+    <img src="{{ $component->getDecoration('label')['value'] }}" alt=""/>
+@endpushonce
